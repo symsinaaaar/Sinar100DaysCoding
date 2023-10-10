a764c45189e790df6f6d2814df39da6f6db4c061
symsinaaaar <osaynar@gmail.com>
2023-10-10 22:20:29 +0800

    Hari Kedua

---
+++ Day2.py
@@ -0,0 +1,12 @@
+#mengenal variabel di python
+#assignment nilai 
+a = 5
+print(a)    
+print("Nilai a =", a)
+#assignment indirect
+b = a
+print(b)
+
+#penamaan menggunakan underscore
+nilai_x = 27
+print(nilai_x)
