7426ae79617cfbe208f5b8cd0ea67a77333b54d3
symsinaaaar <osaynar@gmail.com>
2023-10-14 17:32:36 +0800

    Hari Keenam

---
+++ Day6.py
@@ -0,0 +1,30 @@
+# Operasi Aritmatika
+a = 5
+b = 15
+# Operasi Tambah (+)
+hasil = a + b
+print(a,'+',b,'=', hasil)
+
+#Operasi Pengurangan (-)
+hasil = a - b
+print(a,'-',b,'=', hasil)
+
+#Operasi Perkalian (*)
+hasil = a * b
+print(a,'*',b,'=', hasil)
+
+#Operasi Pembagian (/)
+hasil = a / b
+print(a,'/',b,'=', hasil)
+
+#Operasi Eksponen (Pangkat) **
+hasil = a ** b
+print(a,'**',b,'=', hasil)
+
+#Operasi Modulus (Sisa Pembagian) %
+hasil = a % b
+print(a,'%',b,'=', hasil)
+
+#Operasi Floor Division (Kebalikan Modulus) //
+hasil = a // b
+print(a,'//',b,'=', hasil)
