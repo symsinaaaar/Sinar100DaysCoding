cb538a6b9592aa1e3ea56d5c808356da830725fa
symsinaaaar <osaynar@gmail.com>
2023-10-15 15:22:51 +0800

    Hari Ketujuh

---
+++ Day7.py
@@ -0,0 +1,21 @@
+# Lanjutan Operasi Aritmatika
+
+# Prioritas Operasi, Operational Precedence
+'''
+    1. ()
+    2. **
+    3. *, /, %, //
+    4. +, -
+'''
+x = 7
+y = 5
+z = 3
+
+hasil = x + y * z
+print(x,'+',y,'*',z, '=', hasil)
+
+hasil = (x + y) * z
+print(x,'+',y,'*',z, '=', hasil)
+
+hasil = x ** y * z + x / y - y % z // x
+print(x,'**',y,'*',z,'+',x,'/',y,'-',y,'%',z,'//',x,'=', hasil)
