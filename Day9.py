4c8cfe88a1e8bfb368a4a4c0b82b8de7327d9559
symsinaaaar <osaynar@gmail.com>
2023-10-17 19:49:18 +0800

    Operasi Komparasi

---
+++ Day9.py
@@ -0,0 +1,52 @@
+##Operasi Komparasi
+# Setiap hasil dari operasi komperasi adalah boolean
+
+# >, <, >=, <=, ==, !=, is, is not
+
+a = 5
+b = 3
+
+# lebih besar dari >
+print('========== lebih besar dari (>)')
+hasil = a > 4
+print(a,'>',4,'=', hasil)
+hasil = b > 4
+print(b,'>',4,'=', hasil)
+
+# kurang dari <
+print('========== kurang dari (<)')
+hasil = a < 4
+print(a,'<',4,'=', hasil)
+hasil = b < 4
+print(b,'<',4,'=', hasil)
+
+# lebih besar dari sama dengan >=
+print('========== lebih besar dari sama dengan (>=)')
+hasil = a >= 4
+print(a,'>=',4,'=', hasil)
+hasil = b >= 4
+print(b,'>=',4,'=', hasil)
+
+# kurang dari sama dengan >
+print('========== kurang dari sama dengan (<=)')
+hasil = a <= 4
+print(a,'<=',4,'=', hasil)
+hasil = b <= 4
+print(b,'<=',4,'=', hasil)
+
+# sama dengan ==
+print('========== sama dengan (==)')
+hasil = a == 4
+print(a,'==',4,'=', hasil)
+hasil = b == 4
+print(b,'==',4,'=', hasil)
+
+# tidak sama dengan !=
+print('========== sama dengan (!=)')
+hasil = a != 4
+print(a,'!=',4,'=', hasil)
+hasil = b != 4
+print(b,'!=',4,'=', hasil)
+
+
+
