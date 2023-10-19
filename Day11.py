dc791383398a7d7ec1f37e51e3e7c227d7a438a7
symsinaaaar <osaynar@gmail.com>
2023-10-19 23:33:08 +0800

    komparasi dan logika

---
+++ Day11.py
@@ -0,0 +1,17 @@
+## Komparasi dan Logika ##
+# membuat gabungan area rentang dari angka
+# +++++3------10+++++
+inputUser = float(input("masukan angka yang bernilai\nkurang dari 3 \natau \nlebih besar dari 10\n:"))
+
+# +++++3--------
+# memeriksa angka kurang dari 3
+isKurangDari = (inputUser < 3)
+print("Kurang dari 3 =", isKurangDari)
+
+# ----------10++++++++++
+# memeriksa angka lebih dari 10
+isLebihDari = (inputUser > 10)
+print("Lebih dari 10 =", isLebihDari)
+
+
+
