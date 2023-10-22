6f1776eabdf2e2721517cb826b0c125adfd64185
symsinaaaar <osaynar@gmail.com>
2023-10-22 13:11:45 +0800

    Operator Assignment

---
+++ Day13.py
@@ -0,0 +1,69 @@
+## Operator Assignment
+# Operasi yang dapat dilakukan dengan penyingkatan
+#operasi ditambah dengan assignment
+
+a = 3 # adalah assignment
+print("nilai a =", a)
+
+a += 1 # artinya adalah a = a + 1
+print("niali a+= 1, niali a menjadi",a)
+
+a -= 2 # artinya adalah a = a - 2
+print("niali a-= 2, niali a menjadi",a)
+
+a *= 7 # artinya adalah a = a * 7
+print("niali a*= 7, niali a menjadi",a)
+
+a /= 5 # artinya adalah a = a / 5
+print("niali a/= 5, niali a menjadi",a)
+
+# modulus dan floor division
+b = 5 
+print("\nnilai b=",b)
+b %= 2
+print("nilai b %= 2, nilai b menjadi",b)
+
+b //= 2
+print("nilai b //= 2, nilai b menjadi",b)
+
+
+# pangkat dan eksponen
+a = 3
+print("nilai a =", a)
+a **= 2
+print("nilai b **= 2, nilai b menjadi",b)
+
+# operasi bitwise
+# OR
+c = True
+print("\nnilai c =",c)
+c |= False
+print("nilai c |= False, nilai c menjadi",c)
+c = False
+print("\nnilai c =",c)
+c |= False
+print("nilai c |= False, nilai c menjadi",c)
+
+# AND
+c = True
+print("\nnilai c =",c)
+c &= False
+print("nilai c &= False, nilai c menjadi",c)
+c = True
+print("\nnilai c =",c)
+c &= True
+print("nilai c &= True, nilai c menjadi",c)
+
+# XOR
+c = True
+print("\nnilai c =",c)
+c ^= False
+print("nilai c ^= False, nilai c menjadi",c)
+c = True
+print("\nnilai c =",c)
+c ^= True
+print("nilai c ^= True, nilai c menjadi",c)
+
+
+
+
