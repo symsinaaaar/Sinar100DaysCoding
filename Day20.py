5572d2468b580d502b7d8e6d53c79db7ddf1969d
symsinaaaar <osaynar@gmail.com>
2023-10-30 00:06:57 +0800

    If Else Statment

---
+++ Day20.py
@@ -0,0 +1,24 @@
+# If Else Statment
+
+# 1.If nya
+# 2.kondisinya
+# 3.aksinya
+
+nama = input("Siapa nama anda? ")
+
+# program if inline
+if nama=="Sinar" : print("Kamu cantik sekali !!!")
+print(f"Terima kasih {nama}")
+
+# program if indentation
+if nama=="Samsinar":
+    print("Kamu lebih cantik")
+    print("Kamu juga cantik")
+print(f"Terima kasih {nama}")
+
+# Else Statment
+if nama=="Narr":
+    print("Hey what's up")
+else:
+    print("Haloo")
+print("akhir dari program")
---
+++ Day21.py
@@ -0,0 +1,14 @@
+# ELIF = Else If Statment
+
+nama = input("Siapa nama Anda? ")
+
+if nama == "Sinar":
+    print("Hai, sudah makan hari ini?")
+elif nama == "Samsinar":
+    print("Hai, sudah belajar hari ini?")
+elif nama == "Narr":
+    print("Hai, mau jalan kemana lagi?")
+else:
+    print("Anda siapa??")
+
+print("Ini akhir dari program")
