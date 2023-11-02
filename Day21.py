83bbe4409ff2eda1258fd4281eba1a85e509ae10
symsinaaaar <osaynar@gmail.com>
2023-11-02 18:57:29 +0800

    Else If Statment

--- Day21.py
+++ Day21.py
@@ -2,13 +2,13 @@
 
 nama = input("Siapa nama Anda? ")
 
-if nama == "Sinar":
-    print("Hai, sudah makan hari ini?")
-elif nama == "Samsinar":
-    print("Hai, sudah belajar hari ini?")
-elif nama == "Narr":
-    print("Hai, mau jalan kemana lagi?")
+if nama == "Sinar": # kondisi 1
+    print("Hai, sudah makan hari ini?") # aksi true 1
+elif nama == "Samsinar": # kondisi 2
+    print("Hai, sudah belajar hari ini?") # aksi true 2
+elif nama == "Narr": # kondisi 3
+    print("Hai, mau jalan kemana lagi?") # aksi true 3
 else:
-    print("Anda siapa??")
+    print("Anda siapa??") # aksi false
 
 print("Ini akhir dari program")
