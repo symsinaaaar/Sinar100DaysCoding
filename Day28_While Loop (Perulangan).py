29583a5bdf85fd34366a8088a82f10a44bba473d
symsinaaaar <osaynar@gmail.com>
2023-11-05 23:23:22 +0800

    While Loop (Perulangan)

---
+++ Day28_While Loop (Perulangan).py
@@ -0,0 +1,15 @@
+# While loop
+
+angka = 0
+while angka < 5:
+    angka += 1
+    print("Sinar")
+print("Stop")
+
+angka = 0
+print(f"angka sekarang -> {angka}")
+while angka < 5:
+    angka += 1
+    print(f"angka sekarang -> {angka}")
+    print("Sinar kece maximal!")
+print("Stop")
