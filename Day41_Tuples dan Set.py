98870cf2d6e4ea86e15840cbae94170f5e169bec
symsinaaaar <osaynar@gmail.com>
2023-11-18 23:31:07 +0800

    Tuples dan Set

---
+++ Day41_Tuples dan Set.py
@@ -0,0 +1,14 @@
+# list 
+# menggunakan kurung siku
+data_list = [10, 2, 4, 3, 2] 
+print(data_list)
+
+# tuples
+data_tuples = (7, 8, 9, 10)
+print(data_tuples) # data tupples tidak bisa di ubah (assigment)
+
+# sets (Himpunan)
+data_sets = {10,4,3,2,4,7,6,5} # data sets tidak punya index
+print(data_sets)
+
+print(data_sets[0])
