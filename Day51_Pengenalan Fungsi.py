d61484c197070580890204209e2f935d5724ed27
symsinaaaar <osaynar@gmail.com>
2023-11-28 20:46:32 +0800

    Pengenalan Fungsi

---
+++ Day51_Pengenalan Fungsi.py
@@ -0,0 +1,18 @@
+'''Membuat fungsi'''
+
+def Hello_World():
+    #fungsi menampilkan Hello World
+    print("Hello World")
+    print("Kepada Sinar")
+    print("Dan juga Kepada Juli")
+
+Hello_World()
+Hello_World()
+
+
+
+def fungsi():
+    # pemanggilan fungsi harus setelah dibuat
+    print("Ini adalah fungsi")
+    
+fungsi()
